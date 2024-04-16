92e6cc801be20a0a1fbaf97196cdf015db000e9c
bigmoby <ing.fabiomauro@gmail.com>
2024-04-16 19:13:02 +0200

    fixing fan_value for value 9, undocumented value assumed to be High

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "2.0.11"
+__version__ = "2.0.12"
 
 setup(
     name="pyfujitsugeneral",
@@ -22,4 +22,5 @@ setup(
     packages=["pyfujitsugeneral"],
     include_package_data=True,
     install_requires=["requests", "certifi", "chardet", "idna", "urllib3", "numpy"],
+    python_requires=">=3.12",
 )
